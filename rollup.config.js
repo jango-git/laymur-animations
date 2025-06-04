5b32daa2e533f1bee8a8c0be9c5e2cef1b1cc59d
Jango <jango@jango.pro>
2025-06-04 14:02:17 +0200

    Enable terser minification and update dependencies
    Version bump to 0.1.8 and change external dependency from three to gsap

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,5 @@
 import { nodeResolve } from "@rollup/plugin-node-resolve";
-// import terser from "@rollup/plugin-terser";
+import terser from "@rollup/plugin-terser";
 import typescript from "rollup-plugin-typescript2";
 
 export default {
@@ -15,33 +15,33 @@ export default {
       tsconfig: "tsconfig.json",
       useTsconfigDeclarationDir: true,
     }),
-    // terser({
-    //   compress: {
-    //     arrows: true,
-    //     arguments: true,
-    //     booleans_as_integers: true,
-    //     dead_code: true,
-    //     drop_console: true,
-    //     drop_debugger: true,
-    //     ecma: 2020,
-    //     keep_fargs: false,
-    //     passes: 3,
-    //     toplevel: true,
-    //     pure_getters: true,
-    //     unsafe: true,
-    //     unsafe_arrows: true,
-    //     unsafe_methods: true,
-    //   },
-    //   format: {
-    //     comments: false,
-    //     ecma: 2020,
-    //   },
-    //   mangle: {
-    //     properties: {
-    //       regex: /^_/,
-    //     },
-    //   },
-    // }),
+    terser({
+      compress: {
+        arrows: true,
+        arguments: true,
+        booleans_as_integers: true,
+        dead_code: true,
+        drop_console: true,
+        drop_debugger: true,
+        ecma: 2020,
+        keep_fargs: false,
+        passes: 3,
+        toplevel: true,
+        pure_getters: true,
+        unsafe: true,
+        unsafe_arrows: true,
+        unsafe_methods: true,
+      },
+      format: {
+        comments: false,
+        ecma: 2020,
+      },
+      mangle: {
+        properties: {
+          regex: /^_/,
+        },
+      },
+    }),
   ],
-  external: ["three"],
+  external: ["gsap"],
 };
