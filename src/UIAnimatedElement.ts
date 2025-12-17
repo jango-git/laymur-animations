2b00b0191316cc7112a7faf1a22bfe35af449641
Jango <jango@jango.pro>
2025-12-17 11:57:49 +0100

    Add support for the new version of the library

--- src/UIAnimatedElement.ts
+++ src/UIAnimatedElement.ts
@@ -4,8 +4,8 @@ import type { UIColor, UIMicro } from "laymur";
  * Interface for UI elements that can be animated.
  */
 export interface UIAnimatedElement {
-  /** Element alpha value */
-  color: UIColor;
   /** Micro transformation object */
   micro: UIMicro;
+  /** Element alpha value */
+  color: UIColor;
 }
